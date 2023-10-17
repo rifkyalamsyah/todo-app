7c3f0f81f1cfd95dfeb783f1914d19a8bc07bcf4
Rifky Alamsyah <rifkyalamsyah@Rifkys-MacBook-Pro.local>
2023-10-17 11:29:36 +0700

    add web storage: local storage

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
     todos.push(todoObject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
   }
 
   function generateId() {
@@ -111,6 +112,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       todoTarget.isCompleted = true;
       document.dispatchEvent(new Event(RENDER_EVENT));
+      saveData();
     }
 
     // find todo list
@@ -131,6 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       todos.splice(todoTarget, 1);
       document.dispatchEvent(new Event(RENDER_EVENT));
+      saveData();
     }
 
     //   undo task from completed
@@ -141,6 +144,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       todoTarget.isCompleted = false;
       document.dispatchEvent(new Event(RENDER_EVENT));
+      saveData();
     }
 
     // find todo index
@@ -155,5 +159,48 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     return container;
+  }
+
+  // save data
+  function saveData() {
+    if (isStorageExist()) {
+      const parsed = JSON.stringify(todos);
+      localStorage.setItem(STORAGE_KEY, parsed);
+      document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+  }
+
+  const SAVED_EVENT = 'saved-todo';
+  const STORAGE_KEY = 'TODO_APPS';
+
+  function isStorageExist() /* boolean */ {
+    if (typeof Storage === undefined) {
+      alert('Browser kamu tidak mendukung local storage');
+      return false;
+    }
+    return true;
+  }
+
+  document.addEventListener(SAVED_EVENT, function () {
+    console.log(localStorage.getItem(STORAGE_KEY));
+  });
+
+  // load data from storage
+  function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if (data !== null) {
+      for (const todo of data) {
+        todos.push(todo);
+      }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  // load data
+  if (isStorageExist()) {
+    loadDataFromStorage();
   }
 });
